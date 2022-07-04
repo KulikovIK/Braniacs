e2c0abbe5257980eb2ab7539a3a27dd0305ce65a
Kulikov I.K <ilyatrg@gmail.com>
2022-07-04 20:27:36 +0500

    celery, cache and logs

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,29 +1,33 @@
 from django.urls import path
-from mainapp.views import MainView, ContacntsView, CoursesView, DocsView, LoginView, NewsView, SearchView, NewsDetailView, NewsDeleteView, NewsCreateView, NewsUpdateView, CourseDetailView, CourseFeedbackCreateView
+from mainapp import views
+from django.views.decorators.cache import cache_page
 from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', MainView.as_view(), name='index'),
-    path('contacts/', ContacntsView.as_view(), name='contacts'),
+    path('', views.MainView.as_view(), name='index'),
+    path('contacts/', views.ContacntsView.as_view(), name='contacts'),
+    path('doc_site/', views.DocsView.as_view(), name='docs'),
+    path('login/', views.LoginView.as_view(), name='login'),
 
     # Courses
-    path('courses/', CoursesView.as_view(), name='courses'),
-    path('courses/<int:pk>/detail/', CourseDetailView.as_view(), name='courses_detail'),
-    path('courses/feedback/', CourseFeedbackCreateView.as_view(), name='course_feedback'),
-
-    path('doc_site/', DocsView.as_view(), name='docs'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('courses/', cache_page(360)(views.CoursesView.as_view()), name='courses'),
+    path('courses/<int:pk>/detail/', views.CourseDetailView.as_view(), name='courses_detail'),
+    path('courses/feedback/', views.CourseFeedbackCreateView.as_view(), name='course_feedback'),
 
     # News
-    path('news/', NewsView.as_view(), name='news'),
-    path('news/<int:pk>/detail/', NewsDetailView.as_view(), name='news_detail'),
-    path('news/<int:pk>/delete/', NewsDeleteView.as_view(), name='news_delete'),
-    path('news/<int:pk>/update/', NewsUpdateView.as_view(), name='news_update'),
-    path('news/create/', NewsCreateView.as_view(), name='news_create'),
+    path('news/', cache_page(360)(views.NewsView.as_view()), name='news'),
+    path('news/<int:pk>/detail/', views.NewsDetailView.as_view(), name='news_detail'),
+    path('news/<int:pk>/delete/', views.NewsDeleteView.as_view(), name='news_delete'),
+    path('news/<int:pk>/update/', views.NewsUpdateView.as_view(), name='news_update'),
+    path('news/create/', views.NewsCreateView.as_view(), name='news_create'),
+
+    # Logs
+    path('logs/', views.LogView.as_view(), name='logs_list'),
+    path('logs/download/', views.LogDownloadView.as_view(), name='logs_download'),
 
     # other
-    path('search/', SearchView.as_view(), name='search'),
-]
+    path('search/', views.SearchView.as_view(), name='search'),
+  ]
 
